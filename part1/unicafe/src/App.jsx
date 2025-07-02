194ec7a9b110fa89e7a98ea6d9fdec2226b27168
jarimus <jari.n.ahonen@gmail.com>
2025-07-02 20:21:57 +0300

    exercise 1.11 finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,22 +18,25 @@ const Statistics = ( {good, neutral, bad} ) => {
   return (
     <>
       <h2>statistics</h2>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={good + bad + neutral} />
-      <StatisticsLine text="average" value={(good - bad) / (good + neutral + bad)} />
-      <StatisticsLine text="positive" value={ good / (good + bad + neutral) * 100 + " %" } />
+      <table>
+        <tbody>
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={good + bad + neutral} />
+            <StatisticsLine text="average" value={(good - bad) / (good + neutral + bad)} />
+            <StatisticsLine text="positive" value={ good / (good + bad + neutral) * 100 + " %" } />
+        </tbody>
+      </table>
     </>
-
   )
 }
 
 const StatisticsLine = ( {text, value} ) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+      <tr>
+        <td>{text}</td><td>{value}</td>
+      </tr>
   )
 }
 
@@ -59,7 +62,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <>
       <h2>give feedback</h2>
       <p>
         <Button onClick={handleGoodClick} text="good" />
@@ -67,7 +70,7 @@ const App = () => {
         <Button onClick={handleBadClick} text="bad" />
       </p>
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    </>
   )
 }
 
